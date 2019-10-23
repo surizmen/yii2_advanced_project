ee7028aee6988ba0cd763aaf1a600f7954a47ba8
surizmen <karach.kirill@bk.ru>
2019-10-23 18:56:17 +0700

    thirth commit

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,11 +13,22 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        "css/bootstrap.css",
     ];
     public $js = [
+        //'assets/js/jquery.min.js',
+        'assets/js/jquery-ui.js',
+        'assets/js/jquery.dataTables.min.js',
+        'assets/js/jquery-ui.multidatespicker.js',
+        'assets/js/chosen.jquery.js',
+        'assets/js/chosen.jquery.js',
+        'assets/js/my-custom.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
+    public $jsOptions = array(
+        'position' => \yii\web\View::POS_HEAD
+    );
 }
--- backend/controllers/TestController.php
+++ backend/controllers/TestController.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace backend\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+
+class TestController extends Controller
+{
+    public function actionSay($target = 'World')
+    {
+        return $this->render('say', ['target' => $target]);
+    }
+}
