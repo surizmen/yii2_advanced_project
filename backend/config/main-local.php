e766363c66cd3752b2f00717dda6d26d0055fd5a
surizmen <karach.kirill@bk.ru>
2019-10-23 18:47:04 +0700

    second-commit

---
+++ backend/config/main-local.php
@@ -0,0 +1,33 @@
+<?php
+
+$config = [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            // advanced_yii2 is the database name
+            'dsn' => 'mysql:host=localhost:5432;dbname=postgres',
+            'username' => 'postgres',
+            'password' => 'arts123',
+            'charset' => 'utf8',
+        ],
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'uKTQxTdNqIX-nPx85GQCJiLmqt4VJI3E',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
